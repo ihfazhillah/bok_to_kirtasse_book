f8ae37e64ecef84aa6266fc4362386beb474f8c2
ihfazhillah <github.com>
2016-07-20 20:12:39 +0700

    sudah bisa membuat file bookinfo.info

--- bok2kirtass.py
+++ bok2kirtass.py
@@ -29,8 +29,19 @@ def main_parser(csv_obj):
                         "betaka", "author", "cat"])
     return bookinfo(bk_id, title, betaka, author, category)
 
+def make_bookinfo_xml(main_object):
+    template = """
+<?xml version='1.0' encoding='UTF-8'?>
+<dataroot>
+<groupe title="{title}" betaka="{betaka}" author="{author}"/>
+</dataroot>""".strip()
+    bookinfo = template.format(title=main_object.title,
+                           betaka=main_object.betaka.replace("\n", "&#xa;"),
+                           author=main_object.author)
+    with open("bookinfo.info", "w") as f:
+        f.write(bookinfo)
 
 if __name__ == '__main__':
     csv_file = convert_main_table_to_csv("jami.bok")
     csv_obj = read_csv("main.csv")
-    print(main_parser(csv_obj))
+    make_bookinfo_xml(main_parser(csv_obj))
