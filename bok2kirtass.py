f9cf71930d17bfc3e0d4c8183b7692121764d54a
ihfazhillah <github.com>
2016-07-20 19:50:09 +0700

    reading csv dan meretrun list of dict

--- bok2kirtass.py
+++ bok2kirtass.py
@@ -1,17 +1,24 @@
 import csv
 import subprocess
-import tempfile
 
 ENV = {'MDB_JET3_CHARSET':'cp1256'}
 
+
 def convert_main_table_to_csv(fname):
-    command = ['mdb-export', '-d"|*|"', "-Q", fname, 'Main']
-    temp = tempfile.TemporaryFile("w+t")
-    subprocess.Popen(command, stdout=temp, env=ENV).communicate()
-    return temp
+    command = ['mdb-export', '-d|', fname, 'Main']
+    with open("main.csv", "w") as f:
+        subprocess.Popen(command, stdout=f, env=ENV).communicate()
+        return f
+
+def read_csv(fileobj):
+    f = open(fileobj, "r")
+    csv.register_dialect("kirtass", delimiter="|")
+    reader = csv.DictReader(f, dialect='kirtass')
+    return reader
+
 
 if __name__ == '__main__':
     csv_file = convert_main_table_to_csv("jami.bok")
-    csv_file.seek(0)
-    for x in csv_file:
-        print(x.rstrip())
+    readed = read_csv("main.csv")
+    for x in readed:
+        print(x["Betaka"])
